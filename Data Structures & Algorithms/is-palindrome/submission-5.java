f01bb911bf104e2e04ba6904d901bd305443a048
Tanisha Prasad <107425494+tprasad02@users.noreply.github.com>
2026-05-27 21:34:58 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Solution: two pointers moving inwards, stop when they cross
+        // O(n) time, O(n) space
+        // [^a-zA-Z0-9] matches everything that is NOT a letter or number
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "");
+        int j = cleaned.length()-1;
+        for (int i = 0; i < j; i++){
+            String start = cleaned.substring(i, i+1);
+            String end = cleaned.substring(j, j+1);
+            if (start.equalsIgnoreCase(end)) j--;
+            else return false;
+        }
+        return true;
+    }
+}
