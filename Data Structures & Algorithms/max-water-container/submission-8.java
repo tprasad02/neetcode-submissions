7bfd56b085a4996afc4c3cfc75dae4e2949c8b63
Tanisha Prasad <107425494+tprasad02@users.noreply.github.com>
2026-05-30 11:55:19 -0700

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        // Height of the container = min (bar1, bar2) x index of bar2 - index of bar1
+        int i = 0;
+        int j = heights.length-1;
+        int maxHeight = 0;
+        int currHeight;
+        for (int idx = 0; (i < j) && (idx < heights.length); idx++){
+            currHeight = ((Math.min(heights[i], heights[j])) * (j-i));
+            if (currHeight > maxHeight){
+                maxHeight = currHeight;
+            }
+            if (heights[i] <= heights[j]){
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return maxHeight;    
+    }
+}
