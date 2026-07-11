9d61b2775c70162fc54ce39c07227355b0569a61
Tanisha Prasad <107425494+tprasad02@users.noreply.github.com>
2026-07-10 19:19:01 -0700

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        // O(n^2) time, O(1) space
+        
+        int len = 0;
+        int idx = 0;
+        // Try every possible center
+        for (int i = 0; i < s.length(); i++) {
+            // Odd palindrome - one middle letter: "aba"
+            int l = i, r = i;
+            while (l >= 0 && r < s.length() &&
+                s.charAt(l) == s.charAt(r)) {
+                // Found a longer palindrome
+                if (r - l + 1 > len) {
+                    idx = l;
+                    len = r - l + 1;
+                }
+                // Grow outward from the center
+                l--;
+                r++;
+            }
+            // Even palindrome - two middle letters: "abba"
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.length() &&
+                s.charAt(l) == s.charAt(r)) {
+                // Found a longer palindrome
+                if (r - l + 1 > len) {
+                    idx = l;
+                    len = r - l + 1;
+                }
+                // Grow outward from the center
+                l--;
+                r++;
+            }
+        }
+        // Return the longest palindrome found
+        return s.substring(idx, idx + len);
+    }
+}
